811f7cf12283b6375b47064e8e0b42ad55dd2119
Mickeymozy <157571260+Mickeymozy@users.noreply.github.com>
2025-08-29 11:44:42 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -81,7 +81,7 @@ router.get('/', async (req, res) => {
 
                         // Send video thumbnail with caption
                         await KnightBot.sendMessage(userJid, {
-                            image: { url: 'https://img.youtube.com/vi/-oz_u1iMgf8/maxresdefault.jpg' },
+                            image: { url: 'https://ibb.co/PZQqnLFR' },
                             caption: `🎬 *Mickey-tech-bot Full Setup Guide!*\n\n🚀 Bug Fixes + New Commands + Fast AI Chat\n📺 Watch Now: https://youtu.be/-oz_u1iMgf8`
                         });
                         console.log("🎬 Video guide sent successfully");
